ea767efa78d255693670da56adcd82f9ddf6ca42
intern <bawya1098@gmail.com>
2019-05-22 18:41:44 +0530

    added student details

---
+++ src/com/company/Student/Student.java
@@ -0,0 +1,42 @@
+package com.company.Student;
+
+public class Student {
+    int rollno;
+    String name;
+    int subMarks1;
+    int subMarks2;
+    int subMarks3;
+    String department;
+
+    public Student(int rollno, String name, int subMarks1, int subMarks2, int subMarks3, String department) {
+        this.rollno = rollno;
+        this.name = name;
+        this.subMarks1 = subMarks1;
+        this.subMarks2 = subMarks2;
+        this.subMarks3 = subMarks3;
+        this.department = department;
+
+    }
+
+     public  int total() {
+        return  subMarks1 + subMarks2 + subMarks3;
+    }
+
+
+      public  String result() {
+        String result;
+        if (subMarks1 <= 25 || subMarks2 <= 25 || subMarks3 <= 25) {
+            result = "FAIL";
+        } else {
+            result = "PASS";
+
+        }
+        return result;
+    }
+
+
+}
+
+
+
+
