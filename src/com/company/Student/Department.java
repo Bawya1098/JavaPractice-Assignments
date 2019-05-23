ffa9f7ba598e45906c600c7f5929ece205f0ff3c
intern <bawya1098@gmail.com>
2019-05-23 10:49:11 +0530

    Added Department class

---
+++ src/com/company/Student/Department.java
@@ -0,0 +1,13 @@
+package com.company.Student;
+
+public class Department {
+     String name;
+    String hod;
+
+    public Department(String name, String hod) {
+        this.name = name;
+        this.hod = hod;
+    }
+
+
+}
